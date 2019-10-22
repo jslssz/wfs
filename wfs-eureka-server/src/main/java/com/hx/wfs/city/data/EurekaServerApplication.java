863ecee2c230014b58b8aa125ce87ae06562fa2c
jxustcmh <jxrjcmh@163.com>
2019-10-22 12:22:37 +0800

    创建eureka注册中心

---
+++ wfs-eureka-server/src/main/java/com/hx/wfs/city/data/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.hx.wfs.city.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author jxlgcmh
+ * @date 2019-10-22 11:15
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class,args);
+    }
+}
