8488824952f67a4c5b8e0a310be920ff78288b52
jxustcmh <jxrjcmh@163.com>
2019-10-22 18:40:49 +0800

    初步实现天气的缓存

--- wfs-weather-collection/src/main/java/com/hx/wfs/city/data/controller/CollectionDataToRedisController.java
+++ wfs-weather-collection/src/main/java/com/hx/wfs/city/data/controller/CollectionDataToRedisController.java
@@ -1,10 +1,17 @@
 package com.hx.wfs.city.data.controller;
 
+import com.hx.wfs.city.data.util.HttpUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author jxlgcmh
@@ -12,15 +19,48 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class CollectionDataToRedisController {
+    // alibabaAPI
+    public static final String APPCODE = "0793400e45ee448eb0dd34833c17c7e3";
+    public static final String APPID = "75654252";
+    public static final String APPSECRET = "N3JeoicB";
+    public static final String VERSION = "v1";
 
     @Autowired
     private RedisTemplate redisTemplate;
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
-    @RequestMapping("redis")
-    public String collectionDataToRedis() {
-        stringRedisTemplate.opsForValue().set("stringValue","bbb");
+    @RequestMapping("redis/{cityId}")
+    public String collectionDataToRedis(@PathVariable String cityId) {
+        /**
+         * String host = "https://weather01.market.alicloudapi.com";
+         * String path = "/day15";
+         * String method = "GET";
+         * Map<String, String> headers = new HashMap<>();
+         * headers.put("Authorization", "APPCODE " + "");
+         * Map<String, String> querys = new HashMap<>();
+         * querys.put("area", "丽江");
+         * querys.put("areaid", "101230506");
+         */
+
+        String host = "https://www.tianqiapi.com";
+        String path = "/api/";
+        String method = "GET";
+        Map<String, String> querys = new HashMap<>();
+        querys.put("version", VERSION);
+        querys.put("appid", APPID);
+        querys.put("appsecret", APPSECRET);
+        querys.put("cityid", cityId);
+
+        try {
+            HttpResponse response = HttpUtils.doGet(host, path, method, null, querys);
+            //创建不同的key  将key分组
+            String pre = cityId.substring(0, 5)+":";
+            String suff =cityId.substring(5);
+            stringRedisTemplate.opsForValue().set(pre+suff, EntityUtils.toString(response.getEntity()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return "success";
     }
 }
