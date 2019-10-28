e906b4918156b7c6641029713ddbaee81db26101
jxustcmh <cmh960808>
2019-10-28 23:33:36 +0800

    在服务端(API服务的提供方)做服务的熔断

--- wfs-weather-data/src/main/java/com/hx/wfs/weather/data/controller/WeatherDataController.java
+++ wfs-weather-data/src/main/java/com/hx/wfs/weather/data/controller/WeatherDataController.java
@@ -1,11 +1,8 @@
 package com.hx.wfs.weather.data.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.serializer.RedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,9 +19,17 @@ public class WeatherDataController {
 
 
     @RequestMapping(value = "weather/cityId/{cityId}",produces = "application/json;charset=UTF-8")
-    public String getWeatherByCityId(@PathVariable String cityId){
+    @HystrixCommand(fallbackMethod = "hystrixCommand")
+    public String getWeatherByCityId(@PathVariable String cityId) throws Exception {
+        if (cityId.equals("110")) {
+            throw new Exception("非法");
+        }
         String pre = cityId.substring(0, 5)+":";
         String suff =cityId.substring(5);
         return stringRedisTemplate.opsForValue().get(pre + suff);
     }
+    public String hystrixCommand(@PathVariable String cityId) {
+        return "服务熔断"+cityId+"城市数据不存在";
+    }
+
 }
